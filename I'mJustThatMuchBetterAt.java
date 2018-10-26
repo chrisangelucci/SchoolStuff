d345bf69a97db5499fe5d432bf0de2179ca81efe
Dennis The Dude <dennis.lego@aceweb.com>
2018-10-26 13:04:33 -0400

    Create I'mJustThatMuchBetterAt.java

---
+++ I'mJustThatMuchBetterAt.java
@@ -0,0 +1,12 @@
+package DecentProjects;
+
+public class OOFLOOP {
+	public static void main(String[] args) {
+		for (int i = 1; i < 10; i++) {
+			for (int m = 0; m < i; m++) {
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+ }
+}
